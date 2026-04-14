a51fab526758824e05f337a82609aa6efed78015
JustEvil <71156616+EvilG-MC@users.noreply.github.com>
2026-04-14 12:43:28 -0600

    I think I need a better command approach.

--- src/main/java/org/Textureless/kip/KIP.java
+++ src/main/java/org/Textureless/kip/KIP.java
@@ -1,31 +1,22 @@
 package org.Textureless.kip;
 
-import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
-import org.Textureless.kip.builders.KIPCommand;
-import org.Textureless.kip.commands.HelloCommand;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.List;
 
 public final class KIP extends JavaPlugin {
 
-    private final List<KIPCommand> commands = List.of(
-            new HelloCommand()
-    );
-
     @Override
     public void onEnable() {
-        getComponentLogger().info("[KIP] Enabled");
+       saveResource("config.yml", false);
+       saveDefaultConfig();
 
-        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event -> {
-            for (KIPCommand command : commands) {
-                event.registrar().register(command.toBrigadier().build(), command.getDescription());
-            }
-        });
+        getComponentLogger().info(Component.text("KIP has been enabled!").color(NamedTextColor.GREEN));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getComponentLogger().info(Component.text("KIP has been disabled!").color(NamedTextColor.RED));
     }
 }
